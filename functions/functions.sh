69769fb1324e798a34b85d67f55678435ac7ddfb
Andrew T <atenni@users.noreply.github.com>
2025-06-27 08:53:26 +1000

    feat: add a generalised tmux-work func

--- functions/functions.sh
+++ functions/functions.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+
+# Create or attach to a tmux session named "work" with predefined windows
+# - Window 0: TODOs (vim editing todos file)
+# - Window 1: llm 
+# - Window 2: zsh
+tmux-work() {
+  tmux has-session -t work 2>/dev/null
+  if [ $? -eq 0 ]; then
+    tmux attach -t work
+    return
+  fi
+
+  # shellcheck disable=SC2154  # $todos is defined in ./exports/exports.local.sh
+  tmux new-session -d -s work -n TODOs -c "$CODE_DIR/dot-files" "vim \"$todos\""
+  tmux new-window -t work:1 -n llm
+  tmux new-window -t work:2 -n zsh
+  tmux select-window -t work:0
+  tmux attach -t work
+}
